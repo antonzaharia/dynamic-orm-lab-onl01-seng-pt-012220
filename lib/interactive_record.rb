16fb0e43bb04a9abf592bb618b28e249354977aa
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-27 17:08:45 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,7 +52,7 @@ class InteractiveRecord
   end
   
   def self.find_by(attribute)
-    sql = "SELECT * FROM #{self.table_name} VALUES #{attribute.values} "
+    sql = "SELECT * FROM #{self.table_name} VALUES #{attribute.keys[0] = attribute.values[0]} "
   end
   
   
