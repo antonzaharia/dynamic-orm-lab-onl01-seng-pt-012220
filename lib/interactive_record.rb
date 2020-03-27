dce751af70e64bfe78721414e30b8f7d5bb531e0
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-27 17:02:08 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,6 +48,7 @@ class InteractiveRecord
   
   def self.find_by_name(name)
     sql = "SELECT * FROM #{table_name_for_insert} WHERE name = ? LIMIT 1"
+    DB[:conn].execute(sql, name)
   end
   
   
